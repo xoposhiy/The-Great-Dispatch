eaed131c454b9a6919ca524d6656dd4f2788252f
BlaiseEbuth <blaise.ebuth@gmail.com>
2018-01-18 01:30:43 +0100

    Genetical Algorithm
    A solution using a genetical algorithm.

--- The Great Dispatch.cpp
+++ The Great Dispatch.cpp
@@ -1,92 +1,322 @@
 #include <iostream>
+#include <chrono>
+#include <random>
 #include <vector>
+#include <unordered_set>
+#include <algorithm>
+
+#define population 100
 
 using namespace std;
+
+default_random_engine re(chrono::system_clock::now().time_since_epoch().count());
+
+struct timer
+{
+    chrono::high_resolution_clock::time_point t1;
+
+    void start()
+    {
+        t1=chrono::high_resolution_clock::now();
+    }
+
+    int timeIs()
+    {
+        chrono::duration<double,std::milli> time_span=chrono::high_resolution_clock::now()-t1;
+
+        return (int)time_span.count();
+    }
+};
+
+timer t;
 
 struct box
 {
    float weight;
    float volume;
-   int truckId;
-   
-   box()
-   {
-       truckId=-1;
-   }
 };
 
-struct truck
+struct solution
 {
-    float weight;
-    float volume;
+    int score;
+    int boxes[3300];
+    float trucksWeight[100];
+    float trucksVolume[100];
     
-    truck()
+    solution()
     {
-        weight=0;
-        volume=0;
+        score=-1;
+        
+        for(int i=0;i<100;i++)
+        {
+            trucksWeight[i]=0;
+            trucksVolume[i]=0;
+        }
     }
 };
 
-int nextTruck(int truckId)
+bool sortByScore(solution a,solution b)
 {
-    return (truckId+1)%100;
+    return a.score>b.score;
 }
 
-int main()
+struct carPark
 {
     int boxCount;
-    int currentTruck=0;
-    float targetWeight=0;
-    truck trucks[100];
-    vector<box> boxes;
-
-    cin>>boxCount;cin.ignore();
+    unordered_set<int> selectedSolutions;
+    vector<solution> solutions;
+    box boxes[3300];
     
-    for(int i=0;i<boxCount;i++)
+    carPark()
     {
-        box b;
+        cin>>boxCount;cin.ignore();
         
-        cin>>b.weight>>b.volume;cin.ignore();
-        
-        targetWeight+=b.weight;
-        
-        boxes.push_back(b);
-    }
-
-    targetWeight/=100.0;
+        t.start();
     
-    for(int i=0;i<boxCount;i++)
-    {
-        int bestTruck=0;
-        float bestWeightRatio=1000;
-        
-        for(int j=currentTruck;j<100;j++)
+        for(int i=0;i<boxCount;i++)
         {
-            float weightRatio=abs((trucks[j].weight+boxes[i].weight)-targetWeight);
+            cin>>boxes[i].weight>>boxes[i].volume;
+            cin.ignore();
+        }
+    }
+    
+    void populate()
+    {        
+        uniform_int_distribution<int> distrib{0,99};
+        
+        for(int i=0;i<population;i++)
+        {
+            solution s;
+            solutions.push_back(s);
             
-            if(trucks[j].volume+boxes[i].volume<=100 && weightRatio<bestWeightRatio)
+            for(int j=0;j<boxCount;j++)
             {
-                bestTruck=j;
-                bestWeightRatio=weightRatio;
+                int truck=distrib(re);
+                
+                while(solutions[i].trucksVolume[truck]+boxes[j].volume>100)
+                {
+                    truck=distrib(re);
+                }
+
+                solutions[i].boxes[j]=truck;
+                
+                solutions[i].trucksVolume[truck]+=boxes[j].volume;
+                solutions[i].trucksWeight[truck]+=boxes[j].weight;
+            }
+        }
+    }
+    
+    int fitness(int solutionId)
+    {
+        float max=0;
+        float min=100;
+        
+        for(int i=0;i<100;i++)
+        {
+            if(solutions[solutionId].trucksWeight[i]<min)
+            {
+                min=solutions[solutionId].trucksWeight[i];
             }
             
-            if(bestTruck==0 && j==99 && currentTruck!=0)
+            if(solutions[solutionId].trucksWeight[i]>max)
             {
-                j=0;
+                max=solutions[solutionId].trucksWeight[i];
             }
         }
         
-        boxes[i].truckId=bestTruck;
-        trucks[bestTruck].volume+=boxes[i].volume;
-        trucks[bestTruck].weight+=boxes[i].weight;
-        
-        currentTruck=nextTruck(currentTruck);
-    }
-
-    for(int i=0;i<boxCount;i++)
-    {
-        cout<<boxes[i].truckId<<" ";
+        return round(100000.0/(max-min));
     }
     
-    cout<<endl;
+    int sumScores()
+    {
+        int sum=0;
+        
+        for(int i=0;i<population;i++)
+        {
+            sum+=solutions[i].score;
+        }
+        
+        return sum;
+    }
+    
+    int wichSolution(int randomScore)
+    {
+        int count=0;
+        
+        for(int i=0;i<population;i++)
+        {
+            count+=solutions[i].score;
+            
+            if(count>=randomScore)
+            {
+                return i;
+            }
+        }
+    }
+    
+    void selection()
+    {
+         uniform_int_distribution<int> distrib{0,sumScores()};
+         
+         while(selectedSolutions.size()<population/2)
+         {
+             selectedSolutions.insert(wichSolution(distrib(re)));
+         }
+    }
+    
+    void mutation()
+    {
+        unordered_set<int>::iterator it=selectedSolutions.begin();
+        
+        for(it;it!=selectedSolutions.end();it++)
+        {
+            solutions.push_back(solutions[*it]);
+            
+            float max=0;
+            int maxId;
+            
+            for(int i=0;i<100;i++)
+            {
+                if(solutions.back().trucksWeight[i]>max)
+                {
+                    max=solutions.back().trucksWeight[i];
+                    maxId=i;
+                }
+            }
+            
+            float min=max;
+            int minId;
+            
+            for(int i=0;i<100;i++)
+            {
+                if(solutions.back().trucksWeight[i]<min)
+                {
+                    min=solutions.back().trucksWeight[i];
+                    minId=i;
+                }
+            }
+            
+            vector<int> maxBoxes;
+            vector<int> minBoxes;
+            
+            for(int i=0;i<boxCount;i++)
+            {
+                if(solutions.back().boxes[i]==maxId)
+                {
+                    maxBoxes.push_back(i);
+                }
+                
+                if(solutions.back().boxes[i]==minId)
+                {
+                    minBoxes.push_back(i);
+                }
+            }
+            
+            for(int i=0;i<maxBoxes.size();i++)
+            {
+                if(solutions.back().trucksWeight[minId]<solutions.back().trucksWeight[maxId] && solutions.back().trucksVolume[minId]+boxes[maxBoxes[i]].volume<100)
+                {
+                    solutions.back().boxes[maxBoxes[i]]=minId;
+                        
+                    solutions.back().trucksWeight[maxId]-=boxes[maxBoxes[i]].weight;
+                    solutions.back().trucksWeight[minId]+=boxes[maxBoxes[i]].weight;
+                        
+                    solutions.back().trucksVolume[maxId]-=boxes[maxBoxes[i]].volume;
+                    solutions.back().trucksVolume[minId]+=boxes[maxBoxes[i]].volume;
+                    
+                    minBoxes.push_back(maxBoxes[i]);
+                    maxBoxes[i]=-1;
+                }
+            }
+            
+            bool swapped=false;
+            
+            for(int i=0;i<maxBoxes.size();i++)
+            {
+                if(swapped)
+                {
+                    break;
+                }
+                
+                if(maxBoxes[i]!=-1)
+                {                    
+                    for(int j=0;j<minBoxes.size();j++)
+                    {
+                        float newMinWeight=solutions.back().trucksWeight[minId]+boxes[maxBoxes[i]].weight-boxes[minBoxes[j]].weight;
+                        float newMaxWeight=solutions.back().trucksWeight[maxId]-boxes[maxBoxes[i]].weight+boxes[minBoxes[j]].weight;
+                        
+                        float newMinVolume=solutions.back().trucksVolume[minId]+boxes[maxBoxes[i]].volume-boxes[minBoxes[j]].volume;
+                        float newMaxVolume=solutions.back().trucksVolume[maxId]-boxes[maxBoxes[i]].volume+boxes[minBoxes[j]].volume;
+                        
+                        if(newMinVolume<100 && newMaxVolume<100)
+                        {
+                            solutions.back().boxes[maxBoxes[i]]=minId;
+                            solutions.back().boxes[minBoxes[j]]=maxId;
+                            
+                            solutions.back().trucksWeight[maxId]=newMaxWeight;
+                            solutions.back().trucksWeight[minId]=newMinWeight;
+                            
+                            solutions.back().trucksVolume[maxId]=newMaxVolume;
+                            solutions.back().trucksVolume[minId]=newMinVolume;
+                            
+                            swapped=true;
+                            break;
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
+    
+    void load()
+    {
+        populate();
+
+        for(int i=0;i<population;i++)
+        {
+            solutions[i].score=fitness(i);
+        }
+        
+        while(t.timeIs()<49000)
+        {
+            selection();
+            
+            mutation();
+            
+            for(int i=0;i<solutions.size();i++)
+            {
+                if(solutions[i].score=-1)
+                {
+                    solutions[i].score=fitness(i);
+                }
+            }
+            
+            sort(solutions.begin(),solutions.end(),sortByScore);
+            
+            while(solutions.size()!=population)
+            {
+                solutions.pop_back();
+            }
+        }
+    }
+    
+    void start()
+    {
+        for(int i=0;i<boxCount;i++)
+        {
+            cout<<solutions[0].boxes[i]<<" ";
+        }
+        
+        cout<<endl;
+    }
+};
+
+int main()
+{
+    carPark convoy;
+    
+    convoy.load();
+    
+    convoy.start();
 }
